66867d07d0b275db66300c0980436c926df649a5
Antoine Simonot <simonotantoine1@gmail.com>
2021-12-14 14:14:29 +0100

    forms

---
+++ src/Entity/Order.php
@@ -0,0 +1,191 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\OrderRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=OrderRepository::class)
+ * @ORM\Table(name="`order`")
+ */
+class Order
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $client_name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $address;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $phone;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $maximum_date;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Product::class, mappedBy="client_order")
+     */
+    private $products;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Payment::class, mappedBy="client_order")
+     */
+    private $payments;
+
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $state;
+
+    public function __construct()
+    {
+        $this->products = new ArrayCollection();
+        $this->payments = new ArrayCollection();
+    }
+
+
+    public function getState()
+    {
+        return $this->state;
+    }
+
+
+    public function setState($state, $context = [])
+    {
+        $this->state = $state;
+    }
+
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getClientName(): ?string
+    {
+        return $this->client_name;
+    }
+
+    public function setClientName(string $client_name): self
+    {
+        $this->client_name = $client_name;
+
+        return $this;
+    }
+
+    public function getAddress(): ?string
+    {
+        return $this->address;
+    }
+
+    public function setAddress(string $address): self
+    {
+        $this->address = $address;
+
+        return $this;
+    }
+
+    public function getPhone(): ?int
+    {
+        return $this->phone;
+    }
+
+    public function setPhone(int $phone): self
+    {
+        $this->phone = $phone;
+
+        return $this;
+    }
+
+    public function getMaximumDate(): ?\DateTimeInterface
+    {
+        return $this->maximum_date;
+    }
+
+    public function setMaximumDate(\DateTimeInterface $maximum_date): self
+    {
+        $this->maximum_date = $maximum_date;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Product[]
+     */
+    public function getProducts(): Collection
+    {
+        return $this->products;
+    }
+
+    public function addProduct(Product $product): self
+    {
+        if (!$this->products->contains($product)) {
+            $this->products[] = $product;
+            $product->setClientOrder($this);
+        }
+
+        return $this;
+    }
+
+    public function removeProduct(Product $product): self
+    {
+        if ($this->products->removeElement($product)) {
+            // set the owning side to null (unless already changed)
+            if ($product->getClientOrder() === $this) {
+                $product->setClientOrder(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Payment[]
+     */
+    public function getPayments(): Collection
+    {
+        return $this->payments;
+    }
+
+    public function addPayment(Payment $payment): self
+    {
+        if (!$this->payments->contains($payment)) {
+            $this->payments[] = $payment;
+            $payment->setClientOrder($this);
+        }
+
+        return $this;
+    }
+
+    public function removePayment(Payment $payment): self
+    {
+        if ($this->payments->removeElement($payment)) {
+            // set the owning side to null (unless already changed)
+            if ($payment->getClientOrder() === $this) {
+                $payment->setClientOrder(null);
+            }
+        }
+
+        return $this;
+    }
+    
+}
